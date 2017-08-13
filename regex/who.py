e6999e44e348c02f658d9b011409de57875de473
qinhan <1036474541@qq.com>
2017-08-13 17:28:19 +0800

    add

--- regex/who.py
+++ regex/who.py
@@ -1,6 +1,13 @@
 #!/bin/python
 #-*- coding:utf-8 -*-
 import re,os
-with os.popen('who') as fn:
+#with os.popen('who') as fn:
+#    for line in fn:
+#        print(re.split(r'\s\s+|\t',line.rstrip()))
+
+
+with os.popen('tasklist /nh') as fn:
     for line in fn:
-        print(re.split(r'\s\s+|\t',line.rstrip()))
+        if line.strip() is None:
+            continue
+        print(re.findall(r'([\w.]+(?: [\w.]+)*)\s\s+(\d+) ([\w]+)\s\s+(\d)\s\s+([\d,]+ K)',line))
