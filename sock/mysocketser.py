f3e71180bc2d929e8de8bbe49c4f6c9eb265bbd9
qinhan <1036474541@qq.com>
2017-08-17 22:36:52 +0800

    add

--- sock/mysocketser.py
+++ sock/mysocketser.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 from socketserver import (TCPServer as TCP,StreamRequestHandler as SRH)
 from time import ctime
+import time
 host = ''
 port = 8989
 addr = (host,port)
@@ -9,6 +10,7 @@ addr = (host,port)
 class mySocketSer(SRH):
     def handle(self):
         print('...connect from ',self.client_address)
+        time.sleep(10)
         data = self.rfile.readline()
         self.wfile.write(('[%s] %s' %(ctime(),data.decode())).encode())
 
