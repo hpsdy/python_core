0ceeaf49920227790269959dc5eef4500262191e
qinhan <1036474541@qq.com>
2017-08-19 20:49:51 +0800

    add

---
+++ sock/lianxi/2.6/udpcli.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+#-*- coding:utf-8 -*-
+from socket import *
+import traceback
+import sys
+host = 'localhost'
+port = 8888
+bufsize = 1024
+argv = sys.argv
+if len(argv)==2:
+    host = argv[1]
+if len(argv)==3 and argv[2].isdigit():
+    port = int(argv[2])
+
+addr = (host,port)
+sock = socket(AF_INET,SOCK_DGRAM)
+while True:
+    data = input('>>')
+    if data:
+        sock.sendto(data.encode(),addr)
+    while True:
+        data = sock.recvfrom(bufsize)
+        if data:
+            print('return data:%s' % data.decode())
+            break
