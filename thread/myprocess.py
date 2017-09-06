23bdfc4be3f0fa8e73e73f594df55d9995596e78
qinhan <1036474541@qq.com>
2017-09-07 01:14:13 +0800

    add

--- thread/myprocess.py
+++ thread/myprocess.py
@@ -5,14 +5,14 @@ import multiprocessing
 from multiprocessing import Process,Queue,Event
 q = Queue(10)
 def write(e,num):
-    #q.put(num)
-    with e:
-        print('write get event')
+    q.put(num)
+    e.wait()
+    print('write get event')
 def read(e):
-    #num = q.get()
-    #print('num:%s' % num)
-    with e:
-        print('read get event')
+    num = q.get()
+    print('num:%s' % num)
+    e.wait()
+    print('read get event')
 e = Event()
 p1 = Process(target=write,args=(e,10))
 p2 = Process(target=read,args=(e,))
