4b340853fc55d296daf826a0d6630175099438b8
qinhan <1036474541@qq.com>
2017-11-10 17:11:51 +0800

    some modify

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django import views
 from django.http import HttpResponse
-
+import time
 
 def index(request):
-    return HttpResponse('hello world')
+    return HttpResponse('hello world %s' % time.strftime('%Y-%m-%d %H:%M:%S'))
