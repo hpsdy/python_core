945519ae4c6b690ee301b526c24f5f946b5a7061
qinhan <qinhan@baidu.com>
2017-11-10 15:39:51 +0800

    commit

--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField('date published')
+
+class Choice(models.Model):
+    vodes = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
--- mod/index.py
+++ mod/index.py
@@ -1,7 +1,26 @@
-def xt(a,b):
-    print('a:%s' % a)
-    print('b:%s' % b)
-from importlib import import_module,__import__
-import one
-mod = import_module('.data','one')
-#print(mod.info)
+class test(object):
+    def __new__(cls, *args, **kwargs):
+        print(cls, *args, **kwargs)
+        if not hasattr(cls,'instance'):
+            print('first init')
+            cls.instance = super(test,cls).__new__(cls)
+        return cls.instance
+    def __init__(self,num):
+        print('init',self,num)
+test(1)
+test(2)
+test(3)
+print('one',type(test(4)))
+print('two',type(test))
+#type.__new__()
+
+
+class cm(object):
+    def a():
+        b=100
+
+cm.b=999
+o = cm()
+print(o.b)
+o.b=888
+print(cm.b)
