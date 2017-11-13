b1598ffbb8afcf8dea6c86b0b4566a8c4ff9d2b5
qinhan <qinhan@baidu.com>
2017-11-13 11:18:20 +0800

    commit

--- django/mysite/polls/urls.py
+++ django/mysite/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^(?P<question_id>[\d]+)[/]?$',views.detail,name='detail'),
-    url(r'^(?P<question_id>[\d]+)/result[/]?$',views.result,name='detail'),
-    url(r'^(?P<question_id>[\d]+)/vote[/]?$',views.vote,name='detail'),
+    url(r'^(?P<question_id>[\d]+)/result[/]?$',views.result,name='result'),
+    url(r'^(?P<question_id>[\d]+)/vote[/]?$',views.vote,name='vote'),
 
 ]
